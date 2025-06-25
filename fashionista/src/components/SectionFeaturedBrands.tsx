bc1ab1822d4cfaebf92026847b92934c825a41a3
Uudo Sepp <uudo@quantumhorizon.ee>
2025-06-25 12:14:01 +0300

    Imagefix

--- fashionista/src/components/SectionFeaturedBrands.tsx
+++ fashionista/src/components/SectionFeaturedBrands.tsx
@@ -10,22 +10,22 @@
       {/* Logo Row */}
       <div className="flex justify-center items-center gap-20 mb-15 max-w-7xl w-full overflow-hidden">
         <div className="h-18 flex items-center">
-          <Image src="/images/chanel.png" alt="Chanel" width={100} height={40} className="object-contain h-full w-auto" />
+          <Image src="/images/brandNames/chanel.png" alt="Chanel" width={100} height={40} className="object-contain h-full w-auto" />
         </div>
         <div className="h-18 flex items-center">
-          <Image src="/images/louisvuitton.png" alt="Louis Vuitton" width={100} height={40} className="object-contain h-full w-auto" />
+          <Image src="/images/brandNames/louisvuitton.png" alt="Louis Vuitton" width={100} height={40} className="object-contain h-full w-auto" />
         </div>
         <div className="h-18 flex items-center">
-          <Image src="/images/prada.png" alt="Prada" width={100} height={40} className="object-contain h-full w-auto" />
+          <Image src="/images/brandNames/prada.png" alt="Prada" width={100} height={40} className="object-contain h-full w-auto" />
         </div>
         <div className="h-18 flex items-center">
-          <Image src="/images/calvinklein.png" alt="Calvin Klein" width={100} height={40} className="object-contain h-full w-auto" />
+          <Image src="/images/brandNames/calvinklein.png" alt="Calvin Klein" width={100} height={40} className="object-contain h-full w-auto" />
         </div>
         <div className="h-18 flex items-center">
-          <Image src="/images/denim.png" alt="Denim" width={100} height={40} className="object-contain h-full w-auto" />
+          <Image src="/images/brandNames/denim.png" alt="Denim" width={100} height={40} className="object-contain h-full w-auto" />
         </div>
         <div className="h-18 flex items-center">
-          <Image src="/images/adidas.png" alt="Adidas" width={100} height={40} className="object-contain h-full w-auto" />
+          <Image src="/images/brandNames/adidas.png" alt="Adidas" width={100} height={40} className="object-contain h-full w-auto" />
         </div>
       </div>
 
